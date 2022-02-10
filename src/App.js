e119805f2ed8183aa39f329831899dfff335cb29
Mathias Scroccaro Costa <mathias.scroccaro@gmail.com>
2022-02-10 18:52:17 -0300

    Começando a fazer homepage

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect } from 'react';
-import { Navbar, Nav, Container, Row, Col, Badge } from 'react-bootstrap';
+import { Navbar, Nav, Container, Row, Col, Badge, Card, Image, Figure } from 'react-bootstrap';
 
-import logo from './logo.svg';
+const face = require('./static/foto.jpg');
 import './App.css';
 
 const Header = () => {
@@ -9,19 +9,21 @@ const Header = () => {
 		<Navbar bg="dark" variant="dark">
 		  <Container className="d-flex justify-content-center">
 		  <Nav>
-		    <Nav.Link href="/bio">Bio</Nav.Link>
-		    <Nav.Link href="/cv">CV</Nav.Link>
-		    <Nav.Link href="/my_works">Past works</Nav.Link>
+		    <Nav.Link className='px-3' href="/home">home</Nav.Link>
+		    <Nav.Link className='px-3' href="/about">about</Nav.Link>
+		    <Nav.Link className='px-3' href="/previous_work">previous work experience</Nav.Link>
+		    <Nav.Link className='px-3' href="/cv">cv</Nav.Link>
 		  </Nav>
 		  </Container>
-		</Navbar>	
+		</Navbar>
 	);
 }
 
 const Footer = () => {
 	const links_dict = {
 		linkedin: 'https://www.linkedin.com/in/mathias-scroccaro',
-		github: 'https://github.com/mathiasscroccaro'
+		github: 'https://github.com/mathiasscroccaro',
+		homepage: 'http://mathias.dev.br'
 	}
 
 	const links = (
@@ -38,37 +40,107 @@ const Footer = () => {
 		<Container fluid className="d-flex justify-content-center">
 			{links}
 			<Row>
-				Copyleft Ⓐ  - Feito pelo Mathias
+				Copyleft Ⓐ  - Built by Mathias
 			</Row>
 		</Container>
 	);	
 }
 
-const Home = () => {
-	let body = (<div>Olá mundo</div>);
-
-	return (
-	<>
-		{body}
-	</>
+const Title = ({ children }) => {
+	let content = (
+		<Container fluid className='p-5'>
+			<Row>
+				<Col className='d-flex justify-content-center'>
+					<h4>{children}</h4>
+				</Col>
+			</Row>
+		</Container>
 	);
+	return content;
+}
+
+const PostTitle = ({ children }) => {
+	let content = (
+		<Container fluid>
+			<Row className='d-flex justify-content-center'>
+				<Col xs md="auto" className='d-flex justify-content-left'>
+					<h3>{children}</h3>					
+				</Col>
+			</Row>
+		</Container>
+	);
+	return content;
+}
+
+const Content = ({ children }) => {
+	let content = (
+		<Container fluid className='pt-5'>
+			<Row className='d-flex justify-content-center'>
+				<Col xs sm="4" className='d-flex justify-content-center'>
+					<p className="text-center">{children}</p>		
+				</Col>
+			</Row>
+		</Container>
+	);
+	return content;
+}
+
+const Author = ({ children }) => {
+	let image = (
+		<Figure>
+  		<Figure.Image
+    	width={100}
+    	height={100}
+    	alt="171x180"
+    	src={String(face)}
+  		className="rounded-circle"
+			/>
+		</Figure>
+	);
+	let name = (
+		<h4>Mathias S Costa</h4>
+	);
+	//let description = ();
+	return (
+		<Container fluid>
+			<Row className='d-flex justify-content-center'>
+				<Col xs={12} className='d-flex justify-content-center'>
+					{image}
+				</Col>
+				<Col xs={12} className='d-flex justify-content-center'>
+					{name}
+				</Col>
+			</Row>
+		</Container>
+	);
+}
+
+const Home = () => {
+	let body = (
+		<>
+			<Title>~/home</Title>
+			<Author />
+			<PostTitle>Hello, my name is Mathias!</PostTitle>
+		</>
+	);
+
+	return body;
 }
 
 const Page = ({children}) => {
 	return (
-		<>
+		<div className="bg-dark text-white">
 			<Header />
 				{children}
 			<Footer />
-		</>
-		
+		</div>
 	);
 }
 
 const App = () => {
   return (
 		<Page>
-			<Home />
+				<Home />
 		</Page>
 	);
 }
