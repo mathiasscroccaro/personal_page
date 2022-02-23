e191e7eea1825d52186f00ef77f379be4c2652a7
Mathias Scroccaro Costa <mathias.scroccaro@gmail.com>
2022-02-23 20:28:57 -0300

    Modificado icone, title da página e também adicionada pagina de notfound

--- src/App.js
+++ src/App.js
@@ -8,19 +8,24 @@ import { Home } from './pages/home'
 import { About } from './pages/about'
 import { PreviousWork } from './pages/previous_work'
 import { CV } from './pages/cv'
+import { NotFound } from './pages/not_found'
+
 
 const App = () => {
 	document.body.classList.add('bg-dark');
-	document.body.classList.add('text-white');
+	document.body.classList.add('text-white');	
 	
+	document.title = "<Mathias />"
+
 	return (
 		<BrowserRouter>
 			<Routes>
 				<Route path="/" exact element={<Home />} />
 				<Route path="/home" exact element={<Home/>} />
-				<Route path="/about" exact element={<About/>} />
+				<Route path="/about" element={<About/>} />
 				<Route path="/previous_work" exact element={<PreviousWork/>} />
 				<Route path="/cv" exact element={<CV/>} />
+				<Route path="/*" element={<NotFound />} />
 			</Routes>
 		</BrowserRouter>
 	);
---
+++ src/pages/not_found.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+export const NotFound = () => {
+	window.location.href = 'https://http.cat/404'; 
+	return null;
+}
+
+export default NotFound;
